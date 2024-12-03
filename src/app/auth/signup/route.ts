7c89443c01e3f6fce994cc85a8eb2956efbc6299
IsaiahA21 <76446914+IsaiahA21@users.noreply.github.com>
2024-12-03 11:54:00 -0700

    add Dockerfile and docker-compose configuration

--- src/app/auth/signup/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-// createRout
-export async function POST(rew: NextRequest) {
-    const url = new URL (req.url);
-    return NextResponse.redirect(url.origin, 
-        {status: 301}
-    );
-}
